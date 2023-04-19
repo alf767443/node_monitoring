ff12104d838c9986df162c23851712417ef5d9ee
alf767443 <andreluisfranca98@gmail.com>
2023-04-19 21:56:41 +0100

    Diagnostic add

--- src/nodes.py
+++ src/nodes.py
@@ -14,7 +14,7 @@ from ubiquity_motor.msg import MotorState
 from diagnostic_msgs.msg import DiagnosticArray
 
 # Other imports
-import os, bson
+import os, bson, datetime
 from tf.transformations import euler_from_quaternion
 
 # ============== Callback function ============== #
@@ -35,26 +35,34 @@ def q2e(data) -> None:
     # Update the data to storage
     data.update({'pose': {'pose': {'position': data['pose']['pose']['position'], 'orientation': orientation}}})
 
+# To test
 def diag(data) -> None:
-    path =  PATH + "temp/"
-    file = "diag.bjson"
+    path =  PATH + "temp"
+    file = "/diag.bjson"
     if not os.path.exists(path=path):
         os.chmod
         os.makedirs(name=path)
-    file = open(file=path + file, mode='w+b')
+    file = open(file=path + file, mode='bw+')
     # Create directory if it don't exist
     _diag = bson.BSON.decode(file.read())
-    # diag = data['status']
-    _data = {}
-    # for diagnostics in diag:
-    #     ## Verifica se existe a chave
-    #     try:
-    #         _diag[diagnostics['name']]
-    #     except:
-    #         _diag.update({diagnostics['name']: None})
-    #     if (_diag[diagnostics['name']] != diagnostics['level']):
-    #         _diag.update({diagnostics['name']: diagnostics['level']})
-    data = None
+    diag = data['status']
+    _data = []
+    for diagnostics in diag:
+        # Verifica se existe a chave
+        try:
+            _diag[diagnostics['name']]
+        except:
+            _diag.update({diagnostics['name']: None})
+        if (_diag[diagnostics['name']] != diagnostics['level']):
+            _diag.update({diagnostics['name']: diagnostics['level']})
+            diagnostics.update({'dateTime': datetime.datetime.now()})
+            _data.append(diagnostics)
+    file.truncate(0)
+    file.write(_diag)
+    file.close()
+    if( len(_data) == 0):
+        _data = None
+    data = _data
 
 # ============== Nodes ============== #
 
