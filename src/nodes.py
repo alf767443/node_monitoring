3a5565e7461259bc4432002a9ce7e909c7fb5570
alf767443 <andreluisfranca98@gmail.com>
2023-06-07 17:42:33 +0100

    dict clear

--- src/nodes.py
+++ src/nodes.py
@@ -83,7 +83,7 @@ def diffStore(data, node) -> None:
         # Compare the dictionaries
         if compare_dict(_data, _file):
             print('equal')
-            data.update({})
+            data.clear()
         else:
             print('diff')
             file.write(bson.encode(document=_data))
