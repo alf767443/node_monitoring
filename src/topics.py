e4b34ba17fe398b0ec6a30b203c333ef20e2146a
alf767443 <andreluisfranca98@gmail.com>
2023-06-08 10:43:43 +0100

    Add TelloEdu topic (tello_drive)

--- src/topics.py
+++ src/topics.py
@@ -8,9 +8,12 @@ from nav_msgs.msg import *
 from sensor_msgs.msg import *
 from geometry_msgs.msg import *
 from ros_monitoring.msg import *
+from tello_driver.msg import *
+from h264_image_transport.msg import *
 
 # Other imports
 import os, bson, datetime
+from tf.transformations import euler_from_quaternion
 
 # ============== Callback function ============== #
 # The function should always only have one variable 'data', where 'data' is the received message converted to a document
@@ -74,6 +77,21 @@ def diffStore(data, topic) -> None:
     # Close file
     file.close()
 
+def q2e(data) -> None:
+    # Get orientation
+    orientation = data['pose']['pose']['orientation']
+    # Convert
+    (raw, pitch, yaw) = euler_from_quaternion([orientation['x'], orientation['y'], orientation['z'], orientation['w']])
+    # Add in a dictionary
+    orientation = {
+        'raw'     :  raw,
+        'pitch'   : pitch,
+        'yaw'     : yaw,
+    }
+    # Update the data to storage
+    data.update({'pose': {'pose': {'position': data['pose']['pose']['position'], 'orientation': orientation}}})
+
+
 # ============== Nodes ============== #
 
 TOPICS = [
@@ -90,8 +108,33 @@ TOPICS = [
     #     }
     # }
     #############################################################
-
-    
+   
+    # Odometry
+    {
+        'node'    : '/tello/odom',
+        'msg'     : Odometry,
+        'callback': q2e,
+    },
+    # Statues
+    {
+        'node'    : '/tello/status',
+        'msg'     : TelloStatus,
+    },
+    # Inertial Sensor Modules
+    {
+        'node'    : '/tello/imu',
+        'msg'     : Imu,
+    },
+    # Camera info
+    {
+        'node'    : '/tello/image_raw/camera_info',
+        'msg'     : CameraInfo,
+    },
+    # Image RAW
+    {
+        'node'    : '/tello/image_raw/h264',
+        'msg'     : H264Packet,
+    },
     # ConnectionStatus
     {
         'topic'    : '/connectionStatus',
