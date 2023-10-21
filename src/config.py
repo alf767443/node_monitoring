2cdc0aca029c7c6a21e5e614ba46d06dc079f88c
alf767443 <andreluisfrana98@gmail.com>
2023-10-21 12:01:45 +0100

    Upate the config

--- src/config.py
+++ src/config.py
@@ -6,10 +6,10 @@ import os, pymongo
 PATH = os.path.expanduser('~')+'/tempData/'
 
 #============ Mongo ============#
-ROBOT_NAME = 'Tello_01'
-DATALAKE = "CeDRI_TelloEdu"
+ROBOT_NAME = 'Husky_Robot_01'
+DATALAKE = "CeDRI_Husky_Sim"
 DATASOURCE = "CeDRI_robots"
-CLIENT = pymongo.MongoClient('mongodb://192.168.217.183:27017/', 
+CLIENT = pymongo.MongoClient('mongodb://127.0.0.1:27017/', 
                              connectTimeoutMS = 500, 
                              serverSelectionTimeoutMS = 1000, 
                              socketTimeoutMS = 500)
